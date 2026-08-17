c7ef89dbaa3b32769d5a7ec1a9341dc2968591dd
Zeromizer <shawnlyc@gmail.com>
2026-08-17 16:26:22 +0800

    Correct e.MAS 5 branding and plate assets

--- projects/proton-emas5-swift-style/src/components/BrandBar.tsx
+++ projects/proton-emas5-swift-style/src/components/BrandBar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Interactive} from 'remotion';
+import {Img, Interactive, staticFile} from 'remotion';
 
 export const BrandBar: React.FC = () => (
   <Interactive.Div name="Proton e.MAS brand bar" style={{position: 'absolute', top: 64, left: 64, display: 'flex', alignItems: 'center', gap: 18, color: 'white', fontFamily: 'Arial, sans-serif', fontWeight: 900, letterSpacing: 2, fontSize: 30, zIndex: 50}}>
-    <div style={{width: 34, height: 34, border: '5px solid white', borderRadius: 8, rotate: '45deg'}} />
+    <Img name="Authentic Proton tiger-head emblem" src={staticFile('ending/proton-tiger-head-white.png')} style={{width: 42, height: 42, objectFit: 'contain'}} />
     <span>PROTON</span><span style={{fontWeight: 500, opacity: 0.92}}>e.MAS</span>
   </Interactive.Div>
 );
